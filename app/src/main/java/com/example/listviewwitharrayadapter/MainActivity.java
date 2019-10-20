cb809e2f1bd6757494bd6aff2c1ddb4cf11aac58
farhan009 <48472171+farhan009@users.noreply.github.com>
2019-10-20 00:20:47 -0700

    ListView with ArrayAdapter and SearchView

---
+++ app/src/main/java/com/example/listviewwitharrayadapter/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.listviewwitharrayadapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private SearchView searchView;
+    private ListView listView;
+    String[] countryNames;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        searchView = findViewById(R.id.searchView_id);
+        listView = findViewById(R.id.listView_id);
+        countryNames = getResources().getStringArray(R.array.country_names);
+
+        final ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, R.layout.sample_view, R.id.textView_id, countryNames);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String value = countryNames[position];
+                Toast.makeText(MainActivity.this, value + " " + position, Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(MainActivity.this, WelcomePage.class);
+                startActivity(intent);
+            }
+        });
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+    }
+}
